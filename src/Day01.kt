2a269b888b7a9a0fd900f1b7c438780ff2f8455c
Thiyagu GK <thiyagu103@gmail.com>
2024-12-02 14:47:53 +0530

    refactor Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,17 +2,17 @@ import kotlin.math.abs
 
 fun main() {
     fun part1(input: List<String>): Int {
-        val left = input.map { it.split("   ")[0] }.map { it.toInt() }.sorted()
-        val right = input.map { it.split("   ")[1] }.map { it.toInt() }.sorted()
-        val distance = left.zip(right).map { abs(it.second - it.first) }
-        return distance.sum()
+         val values = input.map { it.split("   ") }.map { Pair(it[0].toInt(), it[1].toInt()) }
+        val left = values.map { it.first }.sorted()
+        val right = values.map { it.second }.sorted()
+        return left.zip(right).sumOf { abs(it.second - it.first) }
     }
 
     fun part2(input: List<String>): Int {
-        val left = input.map { it.split("   ")[0] }.map { it.toInt() }
-        val right = input.map { it.split("   ")[1] }.map { it.toInt() }
-        val countOfRight = right.groupingBy { it }.eachCount()
-        return left.sumOf { countOfRight.getOrDefault(it, 0) * it }
+        val values = input.map { it.split("   ") }.map { Pair(it[0].toInt(), it[1].toInt()) }
+        val left = values.map { it.first }
+        val right = values.map { it.second }
+        return left.sumOf { l -> right.count { it == l } * l }
     }
 
     // Read the input from the `src/Day01.txt` file.
